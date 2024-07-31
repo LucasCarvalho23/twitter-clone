1dec11ca8091736a7cafa427649313637529eada
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-07-31 02:20:21 -0300

    initial screen done

--- vendor/MF/Controller/Action.php
+++ vendor/MF/Controller/Action.php
@@ -10,7 +10,7 @@
             $this->view = new \stdClass();
         }
 
-        protected function render($view, $tema) {
+        protected function render($view, $tema = 'layout') {
             $this->view->page = $view;
             require_once "../App/Views/".$tema.".phtml";
         }
