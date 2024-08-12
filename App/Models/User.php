e99e9148d959a0415bb8bf5eef860b1b4d114e65
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-08-12 17:59:30 -0300

    seguir e deixar de seguir funcionando no banco

--- App/Models/User.php
+++ App/Models/User.php
@@ -82,11 +82,21 @@
         }
 
         public function seguirUsuario($id_usuario_seguindo) {
-            echo $id_usuario_seguindo;
+            $query = "INSERT INTO usuarios_seguidores(id_usuario, id_usuario_seguindo) VALUES (:id_usuario, :id_usuario_seguindo)";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(':id_usuario', $this->__get('id'));
+            $stmt->bindValue(':id_usuario_seguindo', $id_usuario_seguindo);
+            $stmt->execute();
+            return true;
         }
 
         public function DeixarSeguirUsuario($id_usuario_seguindo) {
-            echo $id_usuario_seguindo;
+            $query = "DELETE FROM usuarios_seguidores WHERE id_usuario = :id_usuario AND id_usuario_seguindo = :id_usuario_seguindo";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(':id_usuario', $this->__get('id'));
+            $stmt->bindValue(':id_usuario_seguindo', $id_usuario_seguindo);
+            $stmt->execute();
+            return true;
         }
 
     }
